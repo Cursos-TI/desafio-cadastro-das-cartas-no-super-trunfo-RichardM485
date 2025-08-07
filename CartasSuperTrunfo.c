a6dd2271e173b96ab76480e6397b73e70283b8c8
RichardM485 <224180888+RichardM485@users.noreply.github.com>
2025-08-07 03:09:15 +0000

    Desafio Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,21 +2,107 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Nível Aventureiro
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+    char estado1, estado2;
+    char codigodacarta1[4], codigodacarta2[4];
+    char nomecidade1[50], nomecidade2[50];
+    int pop1, pop2;
+    float area1, area2;
+    float pib1, pib2;
+    int numpontosturisticos1, numpontosturisticos2; 
+    //Variáveis criadas para o scrip
+
+    printf("Bem vindo ao cadastro de cartas do jogo SuperTrunfo\n");
+    printf("Vamos começar cadastrando 2 cartas para o jogo, por favor insira os valores: \n");
+    //Apresentação do programa/inicialização.
+
+    printf("\nCARTA 1! \n");
+    printf("Escreva uma letra de 'A' até 'H' para sua carta: ");
+    scanf(" %c", &estado1);
+    //O scrip vai ler e gravar a letra enviada pelo usuário.
+
+    printf("Escreva o código que esta carta irá ter, usando a letra escolhida anteriormente mais uma combinação de número de '01'a'04': ");
+    scanf("%s", codigodacarta1);
+    //O scrip vai ler e gravar a combinação enviada pelo usuário.
+
+    printf("Escreva o nome da cidade: "); 
+    scanf("%s", nomecidade1);
+    //"O fgets() seria o mais recomendado neste caso pois não precisariamos nos limitar a escrever nomes 
+    //de cidades que não possuem nome composto, porém para deixar o codigo mais simples e não utilizar o
+    //while irei utilizar o printf() mesmo"
+
+    printf("Informe a população da cidade: ");
+    scanf("%d", &pop1);
+    //O scrip vai ler e gravar o pop enviada pelo usuário.
+
+    printf("Informe a área em Km² desta cidade: ");
+    scanf("%f", &area1);
+    //O scrip vai ler e gravar a area da cidade.
+
+    printf("Escreva o PIB desta cidade: ");
+    scanf("%f", &pib1);
+    //O scrip vai ler e gravar o valor de PIB que essa cidade possui.
+
+    printf("Escreva quantos pontos turísticos tem essa cidade: ");
+    scanf("%d", &numpontosturisticos1);
+    //O scrip vai ler e gravar a quantidade dos pontos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    //Primeira carta finalizada indo para a carta 2.
+
+    printf("\nCARTA 2!: \n");
+    printf("Escreva uma letra de 'A' até 'H' para sua carta: ");
+    scanf(" %c", &estado2);
+    //O scrip vai ler e gravar a letra enviada pelo usuário.
+
+    printf("Escreva o código que esta carta irá ter, usando a letra escolhida anteriormente mais uma combinação de número de '01'a'04': ");
+    scanf("%s", codigodacarta2);
+    //O scrip vai ler e gravar a combinação enviada pelo usuário.
+
+    printf("Escreva o nome da cidade: "); 
+    scanf("%s", nomecidade2);
+    //"O fgets() seria o mais recomendado neste caso pois não precisariamos nos limitar a escrever nomes 
+    //de cidades que não possuem nome composto, porém para deixar o codigo mais simples e não utilizar o
+    //while irei utilizar o printf() mesmo"
+
+    printf("Informe a população da cidade: ");
+    scanf("%d", &pop2);
+    //O scrip vai ler e gravar o pop enviada pelo usuário.
+
+    printf("Informe a área em Km² desta cidade: ");
+    scanf("%f", &area2);
+    //O scrip vai ler e gravar a area da cidade.
+
+    printf("Escreva o PIB desta cidade: ");
+    scanf("%f", &pib2);
+    //O scrip vai ler e gravar o valor de PIB que essa cidade possui.    
+
+    printf("Escreva quantos pontos turísticos tem essa cidade: ");
+    scanf("%d", &numpontosturisticos2);
+    //O scrip vai ler e gravar a quantidade dos pontos.
+
+    //Resultados compilados.
+    printf("\n----------------------------------------------------------------------\n");
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código da carta: %s\n", codigodacarta1);
+    printf("Nome da cidade: %s\n", nomecidade1);
+    printf("População: %d\n", pop1);
+    printf("Área: %.2f Km²\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Número de Pontos Turísticos: %d", numpontosturisticos1);
+
+    printf("\n----------------------------------------------------------------------\n");
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código da carta: %s\n", codigodacarta2);
+    printf("Nome da cidade: %s\n", nomecidade2);
+    printf("População: %d\n", pop2);
+    printf("Área: %.2f Km²\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Número de Pontos Turísticos: %d", numpontosturisticos2);
 
     return 0;
 }
