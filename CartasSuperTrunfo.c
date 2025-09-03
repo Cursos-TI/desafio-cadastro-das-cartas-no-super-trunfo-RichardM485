ec87ea9a16617011c272043a264b2253aab4193b
RichardM485 <224180888+RichardM485@users.noreply.github.com>
2025-09-03 23:45:11 +0000

    Nível Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,12 +9,13 @@ int main() {
     char estado1, estado2;
     char codigodacarta1[4], codigodacarta2[4];
     char nomecidade1[50], nomecidade2[50];
-    int pop1, pop2;
+    unsigned long int pop1, pop2;
     float area1, area2;
     float pib1, pib2;
     int numpontosturisticos1, numpontosturisticos2;
     float densidadepop1, densidadepop2;
     float pibpcap1, pibpcap2;
+    float Superpoder1, Superpoder2;
     //Variáveis criadas para o scrip
 
     printf("Bem vindo ao cadastro de cartas do jogo SuperTrunfo\n");
@@ -37,7 +38,7 @@ int main() {
     //while irei utilizar o printf() mesmo"
 
     printf("Informe a população da cidade: ");
-    scanf("%d", &pop1);
+    scanf("%lu", &pop1);
     //O scrip vai ler e gravar o pop enviada pelo usuário.
 
     printf("Informe a área em Km² desta cidade: ");
@@ -54,7 +55,8 @@ int main() {
 
     pibpcap1 = pib1 / pop1;
     densidadepop1 = pop1 / area1;
-    //O scrip vai efetuar os calculos de densidade populacional e PIB per capita da carta 1.
+    Superpoder1 = pop1 + area1 + pib1 + numpontosturisticos1 + (1/densidadepop1) + pibpcap1;
+    //O scrip vai efetuar os calculos de densidade populacional,PIB per capita e super poder da carta 1.
     //vale lembrar que se o usuário inserir 0 na população ou na area o programa irá quebrar,
     //mas como foi solicitado para não fazer validações, tive que deixar assim.
         
@@ -76,7 +78,7 @@ int main() {
     //while irei utilizar o printf() mesmo"
 
     printf("Informe a população da cidade: ");
-    scanf("%d", &pop2);
+    scanf("%lu", &pop2);
     //O scrip vai ler e gravar o pop enviada pelo usuário.
 
     printf("Informe a área em Km² desta cidade: ");
@@ -93,7 +95,8 @@ int main() {
 
     pibpcap2 = pib2 / pop2;
     densidadepop2 = pop2 / area2;
-    //O scrip vai efetuar os calculos de densidade populacional e PIB per capita da carta 2.
+    Superpoder2 = pop2 + area2 + pib2 + numpontosturisticos2 + (1/densidadepop2) + pibpcap2;
+    //O scrip vai efetuar os calculos de densidade populacional,PIB per capita e super poder da carta 2.
     //vale lembrar que se o usuário inserir 0 na população ou na area o programa irá quebrar,
     //mas como foi solicitado para não fazer validações, tive que deixar assim.
 
@@ -103,7 +106,7 @@ int main() {
     printf("Estado: %c\n", estado1);
     printf("Código da carta: %s\n", codigodacarta1);
     printf("Nome da cidade: %s\n", nomecidade1);
-    printf("População: %d\n", pop1);
+    printf("População: %lu\n", pop1);
     printf("Área: %.2f Km²\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", numpontosturisticos1);
@@ -115,12 +118,23 @@ int main() {
     printf("Estado: %c\n", estado2);
     printf("Código da carta: %s\n", codigodacarta2);
     printf("Nome da cidade: %s\n", nomecidade2);
-    printf("População: %d\n", pop2);
+    printf("População: %lu\n", pop2);
     printf("Área: %.2f Km²\n", area2);
     printf("PIB: %.2f\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", numpontosturisticos2);
     printf("Densidade Populacional: %.2f hab/Km²\n", densidadepop2);
     printf("PIB per Capita: %.2f reais\n", pibpcap2);
+
+    printf("\n----------------------------------------------------------------------\n");
+    printf("Pontuação de cada carta, caso o resultado for 1 a carta 1 vence, se for 0 a carta 2 vence:\n");
+    printf("A carta que possui a maior população vence: %d\n", (pop1)>(pop2));
+    printf("A carta que possui a maior área vence: %d\n", (area1)>(area2));
+    printf("A carta que possui o maior PIB vence: %d\n", (pib1)>(pib2));
+    printf("A carta que possui o maior número de pontos turísticos vence: %d\n", (numpontosturisticos1)>(numpontosturisticos2));
+    printf("A carta que possui a menor densidade populacional vence: %d\n", (densidadepop1)<(densidadepop2));
+    printf("A carta que possui o maior PIB per capita vence: %d\n", (pibpcap1)>(pibpcap2));
+    printf("A carta que possui o maior Superpoder vence: %d\n", (Superpoder1)>(Superpoder2));
+    printf("----------------------------------------------------------------------\n");
 
     return 0;
 }
